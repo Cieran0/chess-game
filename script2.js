8b8d0b843cddf8f407268166dc4529748a9eb0d2
Cieran O'Neill <cieran999@gmail.com>
2022-11-18 22:36:30 +0000

    movement mostly working!

--- script2.js
+++ script2.js
@@ -7,6 +7,16 @@ window.onload = function() {
     Canvas.drawBoard();
 };
 
+ function contains(array, pos) {
+    var result = false;
+    array.forEach(element => {
+        if(element.x == pos.x && element.y == pos.y) {
+            result = true;
+        }
+    });
+    return result;
+}
+
 class Canvas {
 
     static ctx;
@@ -21,7 +31,9 @@ class Canvas {
         for (let i = 0; i <8; i++) {
             this.drawLine(i);
         }
+        this.drawValidMoves();
         this.drawPieces();
+        document.getElementById("turn").innerHTML = "Turn: " + game.turn;
     }
     
     static drawLine(y) {
@@ -47,6 +59,15 @@ class Canvas {
     static drawPieces() {
         for(let y=0;y<8;y++) for(let x=0;x<8;x++) this.drawPiece(x,y);
     }
+
+    static drawValidMoves() {
+        if(selectedPiece == null) return;
+        var validMoves = game.getValidMoves(selectedPiece);
+        if(validMoves == null || validMoves.length < 1) return;
+        validMoves.forEach(element => {
+            this.drawSquare(element.x,element.y,"cyan");
+        });
+    }
 }
 
 class Game {
@@ -54,10 +75,135 @@ class Game {
         this.board = new Array(8);
         this.blank();
         this.setUpPieces();
+        this.turn = "white";
+    }
+
+    pushIfValid(array, from, to) {
+        var keepGoing = true;
+        if(to.x < 0 || to.y < 0 || to.x > 7 || to.y > 7) return true;
+        if(this.get(to) != null) {
+            if(this.get(to).team == this.get(from).team) return false;
+            else keepGoing=false;
+        }
+        array.push(to);
+        return keepGoing;
+    }
+
+    isEmpty(x,y) {
+        if(x > 7 || x < 0 || y > 7 || y < 0) return true;
+        if(this.board[x][y] == null) return true;
+        return false;
+    }
+
+    pawnMoves(pos) {
+        var validMoves = new Array();
+        var up = (this.get(pos).team == "black")? 1 : -1;
+        if(this.isEmpty(pos.x,pos.y+up)) {
+            if(this.pushIfValid(validMoves,pos,new Position(pos.x,pos.y+up)) && this.get(pos).hasMoved == false) {
+                if(this.isEmpty(pos.x,pos.y+up*2)) {
+                    this.pushIfValid(validMoves,pos,new Position(pos.x,pos.y+up*2)); 
+                }
+            }
+        }
+        if(!this.isEmpty(pos.x+1,pos.y+up)) {
+            this.pushIfValid(validMoves,pos,new Position(pos.x+1,pos.y+up));
+        }
+        if(!this.isEmpty(pos.x-1,pos.y+up)) {
+            this.pushIfValid(validMoves,pos,new Position(pos.x-1,pos.y+up));
+        }
+        return validMoves;
+    } 
+    
+    rookMoves(pos) {
+        var validMoves = new Array();
+        var up = true;
+        var down = true;
+        var left = true;
+        var right = true;
+        for(let i = 1; i < 8; i++) {
+            if(left)  { left  = this.pushIfValid(validMoves,pos,new Position(pos.x-i, pos.y)); }
+            if(right) { right = this.pushIfValid(validMoves,pos,new Position(pos.x+i, pos.y)); }
+            if(up)    { up    = this.pushIfValid(validMoves,pos,new Position(pos.x,   pos.y-i)); }
+            if(down)  { down  = this.pushIfValid(validMoves,pos,new Position(pos.x,   pos.y+i)); }
+        }
+        return validMoves;
+    }
+    
+    bishopMoves(pos) {
+        var validMoves = new Array();
+        var up = true;
+        var down = true;
+        var left = true;
+        var right = true;
+        for(let i = 1; i < 8; i++) {
+            if(i == 0 ) continue;
+            if(left)  { left  = this.pushIfValid(validMoves,pos,new Position(pos.x-i, pos.y + i)); }
+            if(right) { right = this.pushIfValid(validMoves,pos,new Position(pos.x+i, pos.y + i)); }
+            if(up)    { up    = this.pushIfValid(validMoves,pos,new Position(pos.x+i, pos.y - i)); }
+            if(down)  { down  = this.pushIfValid(validMoves,pos,new Position(pos.x-i, pos.y - i)); }
+        }
+        return validMoves;
+    }
+
+    knightMoves(pos) {
+        // TODO: Find better way of checking valid moves for the knight! 
+        var validMoves = new Array();
+        this.pushIfValid(validMoves, pos, new Position(pos.x-1, pos.y - 2));
+        this.pushIfValid(validMoves, pos, new Position(pos.x+1, pos.y - 2));
+        this.pushIfValid(validMoves, pos, new Position(pos.x-1, pos.y + 2));
+        this.pushIfValid(validMoves, pos, new Position(pos.x+1, pos.y + 2));
+        this.pushIfValid(validMoves, pos, new Position(pos.x-2, pos.y - 1));
+        this.pushIfValid(validMoves, pos, new Position(pos.x+2, pos.y - 1));
+        this.pushIfValid(validMoves, pos, new Position(pos.x-2, pos.y + 1));
+        this.pushIfValid(validMoves, pos, new Position(pos.x+2, pos.y + 1));
+        return validMoves;
+    }
+
+    kingMoves(pos) {
+        var validMoves = new Array();
+        for(let i = -1; i < 2; i++) {
+            for(let j = -1; j < 2; j++) {
+                if(i == 0 && j == 0) { continue; }
+                this.pushIfValid(validMoves,pos,new Position(pos.x + i, pos.y + j));
+            }
+        } 
+        return validMoves;
+    }
+    
+    queenMoves(pos) {
+        return this.rookMoves(pos).concat(this.bishopMoves(pos));
+    }
+
+    getValidMoves(pos) {
+        switch (this.get(pos).type) {
+            case "pawn":
+                return this.pawnMoves(pos); 
+            case "rook":
+                return this.rookMoves(pos);
+            case "bishop":
+                return this.bishopMoves(pos);
+            case "knight":
+                return this.knightMoves(pos);
+            case "king":
+                return this.kingMoves(pos);
+            case "queen":
+                return this.queenMoves(pos);
+        }
+        return null;
+    }
+
+    get(pos) {
+        return this.board[pos.x][pos.y];
+    }
+
+    nextTurn() {
+        console.log("nextTurn");
+        this.turn = (this.turn == "black")? "white" : "black";
     }
 
     move(from, to) {
         console.log("Moving!")
+        this.board[from.x][from.y].hasMoved = true;
         this.board[to.x][to.y] = this.board[from.x][from.y];
         this.board[from.x][from.y] = null;
     }
@@ -96,6 +242,7 @@ class Piece {
     constructor(type, team) {
         this.type = type;
         this.team = team;
+        this.hasMoved = false;
     }
 }
 
@@ -111,13 +258,18 @@ class MouseHandler {
 
     static handleMouse(mousePos) {
         if(selectedPiece == null) {
-            if(game.board[mousePos.x][mousePos.y] != null) {
-                selectedPiece = Position.copyPos(mousePos);
+            if(game.get(mousePos) != null) {
+                selectedPiece = (game.get(mousePos).team == game.turn)? Position.copyPos(mousePos) : null;
                 console.log(selectedPiece)
             }
         } else {
-            game.move(selectedPiece,mousePos);
-            selectedPiece = null;
+            if(contains(game.getValidMoves(selectedPiece),mousePos)) {
+                game.move(selectedPiece,mousePos);
+                selectedPiece = null;
+                game.nextTurn();
+            } else {
+                selectedPiece = null;
+            }
         }
         Canvas.drawBoard();
     }
